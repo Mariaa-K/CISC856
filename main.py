1fcb2d72787b295cb9ab3e54e9def9e4d655767c
Maria K <mariakantarjian@gmail.com>
2022-03-17 13:41:42 -0400

    Added PPO

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import environments
 import numpy as np
+from agent import Agent
+from utils import plot_learning_curve
 
 
 class RandomAgent:
@@ -12,23 +14,73 @@ class RandomAgent:
 
 
 def main():
+    N = 20
+    batch_size = 5
+    n_epochs = 4
+    alpha = 0.0003
+    n_games = 300
+    figure_file = 'plots/cartpole.png'
+    learn_iters = 0 # number of times we call learn function
+    avg_score = 0
+    n_steps = 0
+
     dmc_tasks = [('cartpole', 'balance'),
                  ('finger', 'spin'),
                  ('walker', 'walk'),
                  ('cheetah', 'run')
                  ]
-    env = environments.Simulation(
+    env = environments.DMCSimulation(
         domain='cartpole',
         task='balance',
-        # observation_type='position',  # Maria - uncomment this line for easier and faster algorithm confirmation
+        observation_type='position',  # Maria - uncomment this line for easier and faster algorithm confirmation
     )
-    actions = env.env.action_spec()
-    agent = RandomAgent(actions)
-    state = env.time_step
-    for _ in range(50):
-        action = agent.get_action(state)
-        state, reward, terminal = env.step(action)
-        print(f"Reward: {reward}")
+
+    agent = Agent(n_actions = 2, batch_size = batch_size,
+                  alpha = alpha, n_epochs = n_epochs,
+                  input_dims = 6)
+
+    # number of actions: n_actions=env.action_space.n
+    # input dims:  input_dims=env.observation_space.shape
+    # best score = env.reward_range[0]
+    # observation = env.reset()
+
+
+    best_score = env.reward_range[0] #min score for the environment
+    score_history = []
+
+    for i in range(n_games):
+        observation = env.reset() # could be state?
+        done = False
+        score = 0
+        while not done:
+            action, prob, val = agent.choose_action(observation)
+            observation_, reward, done, info = env.step(action)
+            n_steps += 1
+            # score += reward
+            agent.store_transition(observation, action, prob, val, reward, done)
+            if n_steps % N == 0:
+                agent.learn()
+                learn_iters += 1
+            observation = observation_
+        score_history.append(score)
+        avg_score = np.mean(score_history[-100:])
+
+        if avg_score > best_score:
+            best_score = avg_score
+            agent.save_models()
+
+        print('episode', i, 'score %.1f' % score, 'avg score %.1f' % avg_score,
+              'time_steps', n_steps, 'learning_steps', learn_iters)
+    x = [i + 1 for i in range(len(score_history))]
+    plot_learning_curve(x, score_history, figure_file)
+
+    #actions = env.env.action_spec() #action space
+    #agent = RandomAgent(actions)
+    #state = env.time_step
+    #for _ in range(50):
+        #action = agent.get_action(state)
+        #state, reward, terminal = env.step(action)
+        #print(f"Reward: {reward}")
 
 
 main()
