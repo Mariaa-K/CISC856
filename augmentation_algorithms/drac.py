a93fed159dacc88d6b4b4d140c5914e73fdc7813
Jeremy Banks <JBanks@users.noreply.github.com>
2022-03-20 17:38:38 -0400

    gradient updates

--- augmentation_algorithms/drac.py
+++ augmentation_algorithms/drac.py
@@ -46,57 +46,58 @@ class DrAC:
                 data_generator = feed_forward_generator(advantages, self.num_mini_batch)
 
             for sample in data_generator:
-                obs_batch, recurrent_hidden_states_batch, actions_batch, \
-                    value_preds_batch, return_batch, masks_batch, old_action_log_probs_batch, \
-                    adv_targ = sample
-                values, action_log_probs, dist_entropy, _ = self.actor_critic.evaluate_actions(
-                    obs_batch, recurrent_hidden_states_batch, masks_batch,
-                    actions_batch)
+                with tf.GradientTape() as tape:
+                    obs_batch, recurrent_hidden_states_batch, actions_batch, value_preds_batch, \
+                        return_batch, masks_batch, old_action_log_probs_batch, adv_targ \
+                        = sample
+                    values, action_log_probs, dist_entropy, _ = self.actor_critic.evaluate_actions(
+                        obs_batch, recurrent_hidden_states_batch, masks_batch,
+                        actions_batch)
 
-                ratio = tf.exp(action_log_probs - old_action_log_probs_batch)
-                surr1 = ratio * adv_targ
-                surr2 = tf.clip_by_value(ratio,
-                                         1.0 - self.clip_param,
-                                         1.0 + self.clip_param
-                                         ) * adv_targ
-                action_loss = -tf.math.reduce_mean(tf.minimum(surr1, surr2))
+                    ratio = tf.exp(action_log_probs - old_action_log_probs_batch)
+                    surr1 = ratio * adv_targ
+                    surr2 = tf.clip_by_value(ratio,
+                                             1.0 - self.clip_param,
+                                             1.0 + self.clip_param
+                                             ) * adv_targ
+                    action_loss = -tf.math.reduce_mean(tf.minimum(surr1, surr2))
 
-                value_pred_clipped = tf.clip_by_value(value_preds_batch + (values - value_preds_batch),
-                                                      -self.clip_param,
-                                                      self.clip_param)
-                value_losses = (values - return_batch) ** 2
-                value_losses_clipped = (value_pred_clipped - return_batch) ** 2
-                value_loss = 0.5 * tf.math.reduce_mean(tf.maximum(value_losses, value_losses_clipped))
+                    value_pred_clipped = tf.clip_by_value(value_preds_batch + (values - value_preds_batch),
+                                                          -self.clip_param,
+                                                          self.clip_param)
+                    value_losses = (values - return_batch) ** 2
+                    value_losses_clipped = (value_pred_clipped - return_batch) ** 2
+                    value_loss = 0.5 * tf.math.reduce_mean(tf.maximum(value_losses, value_losses_clipped))
 
-                obs_batch_aug = self.aug_func.do_augmentation(obs_batch)
-                obs_batch_id = self.aug_id(obs_batch)
-                _, new_actions_batch, _, _ = self.actor_critic.act(obs_batch_id,
-                                                                   recurrent_hidden_states_batch,
-                                                                   masks_batch)
-                values_aug, actions_log_probs_aug, dist_entropy_aug, _ = self.actor_critic\
-                    .evaluate_actions(obs_batch_aug, recurrent_hidden_states_batch, masks_batch, new_actions_batch)
+                    obs_batch_aug = self.aug_func.do_augmentation(obs_batch)
+                    obs_batch_id = self.aug_id(obs_batch)
+                    _, new_actions_batch, _, _ = self.actor_critic.act(obs_batch_id,
+                                                                       recurrent_hidden_states_batch,
+                                                                       masks_batch)
+                    values_aug, actions_log_probs_aug, dist_entropy_aug, _ = self.actor_critic\
+                        .evaluate_actions(obs_batch_aug, recurrent_hidden_states_batch, masks_batch, new_actions_batch)
 
-                # Compute the Augmented Loss
-                action_loss_aug = -tf.math.reduce_mean(actions_log_probs_aug)
-                value_loss_aug = 0.5 * tf.math.reduce_mean((tf.stop_gradient(values) - values_aug) ** 2)
+                    # Compute the Augmented Loss
+                    action_loss_aug = -tf.math.reduce_mean(actions_log_probs_aug)
+                    value_loss_aug = 0.5 * tf.math.reduce_mean((tf.stop_gradient(values) - values_aug) ** 2)
 
-                # Update actor-critic with PPO and Augmented loss
-                aug_loss = value_loss_aug + action_loss_aug
-                total_loss = (value_loss * self.value_loss_coef +
-                              action_loss -
-                              dist_entropy * self.entropy_coef +
-                              aug_loss * self.aug_coef
-                              )
-                tf.gradients(total_loss)  # TODO: Include inputs in the gradient graph.
+                    # Update actor-critic with PPO and Augmented loss
+                    aug_loss = value_loss_aug + action_loss_aug
+                    total_loss = (value_loss * self.value_loss_coef +
+                                  action_loss -
+                                  dist_entropy * self.entropy_coef +
+                                  aug_loss * self.aug_coef
+                                  )
+                    grad = tape.gradients(total_loss, self.actor_critic.trainable_variables)
 
-                # TODO: Perform gradient clipping
+                    # TODO: Perform gradient clipping
 
-                value_loss_epoch += value_loss.item()
-                action_loss_epoch += action_loss.item()
-                dist_entropy_epoch += dist_entropy.item()
+                    value_loss_epoch += tf.get_static_value(value_loss)
+                    action_loss_epoch += tf.get_static_value(action_loss)
+                    dist_entropy_epoch += tf.get_static_value(dist_entropy)
 
-                if self.aug_func:
-                    self.aug_func.change_randomization_params_all()
+                    if self.aug_func:
+                        self.aug_func.change_randomization_params_all()
 
         num_updates = self.ppo_epoch * self.num_mini_batch
 
