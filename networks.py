2ac77b852cafc08f21ef18c0abfa7aebc8e59897
Emma Ritcey <ritcey.emma@gmail.com>
2022-03-24 13:22:40 -0400

    Update networks.py

--- networks.py
+++ networks.py
@@ -1,7 +1,30 @@
 import tensorflow.keras as keras
-from tensorflow.keras.layers import Dense, Conv2D, ReLU
+from tensorflow.keras.layers import Dense, Conv2D, ReLU, MaxPool2D, Flatten, GRU
+from tensorflow.keras import Sequential
+from tensorflow import nn
+import tensorflow as tf
+
 # from tensorflow import keras
 # from keras.layers import Dense
+
+init_relu_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.
+                        constant_(x, 0), nn.init.calculate_gain('relu'))
+
+
+def apply_init_(modules):
+    """
+    Initialize NN modules
+    """
+    for m in modules:
+        if isinstance(m, nn.Conv2d):
+            nn.init.xavier_uniform_(m.weight)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+            nn.init.constant_(m.weight, 1)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+
 
 
 class ActorNetwork(keras.Model):
@@ -32,6 +55,98 @@ class CriticNetwork(keras.Model):
         q = self.q(x)
 
         return q
+    
+    
+class NNBase(nn.Module):
+    """
+    Actor-Critic network (base class)
+    """
+    def __init__(self, recurrent, recurrent_input_size, hidden_size):
+        super(NNBase, self).__init__()
+
+        self._hidden_size = hidden_size
+        self._recurrent = recurrent
+
+        if recurrent:
+            #self.gru = nn.GRU(recurrent_input_size, hidden_size)
+            self.gru = GRU(hidden_size)
+            for name, param in self.gru.named_parameters():
+                if 'bias' in name:
+                    nn.init.constant_(param, 0)
+                elif 'weight' in name:
+                    nn.init.orthogonal_(param)
+
+    @property
+    def is_recurrent(self):
+        return self._recurrent
+
+    @property
+    def recurrent_hidden_state_size(self):
+        if self._recurrent:
+            return self._hidden_size
+        return 1
+
+    @property
+    def output_size(self):
+        return self._hidden_size
+
+    def _forward_gru(self, x, hxs, masks):
+        if x.size(0) == hxs.size(0):
+            x, hxs = self.gru(x.unsqueeze(0), (hxs * masks).unsqueeze(0))
+            x = x.squeeze(0)
+            hxs = hxs.squeeze(0)
+        else:
+            # x is a (T, N, -1) tensor that has been flatten to (T * N, -1)
+            N = hxs.size(0)
+            T = int(x.size(0) / N)
+
+            # unflatten
+            x = x.view(T, N, x.size(1))
+
+            # Same deal with masks
+            masks = masks.view(T, N)
+
+            # Let's figure out which steps in the sequence have a zero for any agent
+            # We will always assume t=0 has a zero in it as that makes the logic cleaner
+            has_zeros = ((masks[1:] == 0.0) \
+                            .any(dim=-1)
+                            .nonzero()
+                            .squeeze()
+                            .cpu())
+
+            # +1 to correct the masks[1:]
+            if has_zeros.dim() == 0:
+                # Deal with scalar
+                has_zeros = [has_zeros.item() + 1]
+            else:
+                has_zeros = (has_zeros + 1).numpy().tolist()
+
+            # add t=0 and t=T to the list
+            has_zeros = [0] + has_zeros + [T]
+
+            hxs = hxs.unsqueeze(0)
+            outputs = []
+            for i in range(len(has_zeros) - 1):
+                # We can now process steps that don't have any zeros in masks together!
+                # This is much faster
+                start_idx = has_zeros[i]
+                end_idx = has_zeros[i + 1]
+
+                rnn_scores, hxs = self.gru(
+                    x[start_idx:end_idx],
+                    hxs * masks[start_idx].view(1, -1, 1))
+
+                outputs.append(rnn_scores)
+
+            # assert len(outputs) == T
+            # x is a (T, N, -1) tensor
+            x = tf.concat(outputs, dim=0)
+            # flatten
+            x = x.view(T * N, -1)
+            hxs = hxs.squeeze(0)
+
+        return x, hxs   
+    
 
 
 class BasicBlock(keras.Model):
@@ -58,3 +173,52 @@ class BasicBlock(keras.Model):
 
         out += identity
         return out
+    
+    
+    
+    
+class ResNetBase(NNBase):
+    """
+    Residual Network 
+    """
+    def __init__(self, num_inputs, recurrent=False, hidden_size=256, channels=[16,32,32]):
+        super(ResNetBase, self).__init__(recurrent, num_inputs, hidden_size)
+
+        self.layer1 = self._make_layer(channels[0])
+        self.layer2 = self._make_layer(channels[1])
+        self.layer3 = self._make_layer(channels[2])
+
+        self.flatten = self.layer3.Flatten()
+        self.relu = self.flatten.ReLU()
+
+        self.fc = init_relu_(nn.Linear(2048, hidden_size))
+        self.critic_linear = init_(nn.Linear(hidden_size, 1))
+
+        apply_init_(self.modules())
+
+        self.train()
+
+    def _make_layer(self, out_channels, strides=1):
+        model = Sequential()
+
+        model.add(Conv2D(out_channels, kernel_size=3, strides=1))
+        model.add(MaxPool2D(pool_size=3, strides=2, padding=1))
+        model.add(BasicBlock(out_channels))
+        model.add(BasicBlock(out_channels))
+
+        return model
+
+    def forward(self, inputs, rnn_hxs, masks):
+        x = inputs
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        x = self.relu(self.flatten(x))
+        x = self.relu(self.fc(x))
+
+        if self.is_recurrent:
+            x, rnn_hxs = self._forward_gru(x, rnn_hxs, masks)
+
+        return self.critic_linear(x), x, rnn_hxs
